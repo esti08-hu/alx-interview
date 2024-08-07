6c8c19e3f814d8db22450712ff0c780ac99f5205
esti08-hu <estioame@gmail.com>
2024-08-07 14:40:59 +0300

    test

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for 0-minoperations"""
+
+def minOperations(n):
+    """
+    minOperations
+    This function calculates the minimum number of operations needed to get 'n' number of 'H' characters.
+    """
+
+    # all outputs should be at least 2 char
+    if (n < 2):
+        return 0  # If n is less than 2, no operations are needed
+
+    ops, root = 0, 2  # Initialize operations count and root for division check
+    while root <= n:  # Loop until root is greater than n
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root  # Add root to operations count
+            
+            n = n / root  # Update n to reflect division
+            
+            root -= 1  # Decrement root to check for smaller factors
+            
+        root += 1  # Increment root to find next potential factor
+    return ops  # Return the total number of operations needed
